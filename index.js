72f73da7b68f83b9d574b60b03d03b2d04964f88
Valantis Alatsas <valantis.alatsas@gmail.com>
2022-04-13 22:01:22 +0300

    temporary commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
