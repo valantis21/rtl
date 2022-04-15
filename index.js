d7c66d2d17c2c048d9ed6f526e91860be8893f43
Valantis Alatsas <valantis.alatsas@gmail.com>
2022-04-15 08:47:13 +0300

    movable shared notes

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 80;
 
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + "/index.html")
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+  socket.on('postit created', coords => {
+    io.emit("create postit", coords)
   });
+  socket.on("text changed", text => {
+    socket.broadcast.emit("change text", text)
+  });
+  socket.on("close postit", id => {
+    socket.broadcast.emit("close postit", id)
+  });
+  socket.on("window move", newPos => {
+    socket.broadcast.emit("window move", newPos)
+  })
 });
 
 http.listen(port, () => {
